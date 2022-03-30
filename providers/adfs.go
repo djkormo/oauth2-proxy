af1e2144669482080837b3e77735ac6e37ffa49b
Krzysztof Pudłowski <djkormo@gmail.com>
2022-03-30 15:08:14 +0200

    else

--- providers/adfs.go
+++ providers/adfs.go
@@ -85,6 +85,9 @@ func (p *ADFSProvider) EnrichSession(ctx context.Context, s *sessions.SessionSta
 	if len(s.Groups) > 0 {
            fmt.Println("djkormo -> Found groups: ",s.Groups)
 	}
+	else {
+	  fmt.Println("djkormo -> No groups for emai: ",s.Email)
+	}	
 	return nil
 }
 
